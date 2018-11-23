77dd986fca34b74e5d6c52d9e3dbbd5f5ddb392b
Jonathan Henry <JSHenry@Muhlenberg.edu>
2018-11-23 00:07:55 -0500

    about to screw up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+    
+    before_save { self.email = email.downcase }
+
+    validates :name,  presence: true, length: { maximum: 50 }
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i 
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, uniqueness: { case_sensitive: false }
+    
+    #VALID_PASSWORD_REGEX = /\A[\w+\-.]/i
+    #validates :password, presence: true, length: { maximum: 50 }, format: { with: VALID_PASSWORD_REGEX }
+    #validates :password, presence: true, length: { maximum: 50 }, length:{minimum: 5}
+
+    #validates :password_confirmation,  presence: true, format: {with: password}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_001200) do
+ActiveRecord::Schema.define(version: 2018_11_23_045140) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_001200) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    #t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
