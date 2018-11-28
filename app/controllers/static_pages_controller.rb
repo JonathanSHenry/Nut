e62fe0f71bd06231c2d092565890dbbaa61fa494
Jonathan Henry <JSHenry@Muhlenberg.edu>
2018-11-28 16:16:44 -0500

    added search bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,16 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-  def search
+  def create_restaurant
+  end
+
+  def search  
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].downcase  
+      @results = Restaurant.all.where("lower(name) LIKE :search", search: @parameter) # change to match restaurants micropost database setup, possibly tag items
+    end  
   end
 
   def login
