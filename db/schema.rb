cf8fa73bb89fd0bac23e748afb90a52ce817c2a8
eeinhorn <ee237534@muhlenberg.edu>
2018-11-27 23:12:46 -0500

    implemented advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_054022) do
+ActiveRecord::Schema.define(version: 2018_11_28_031032) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_054022) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
