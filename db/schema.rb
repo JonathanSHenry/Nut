05611967d1e624ed6b0852aecdfa4f0e387d5e6c
eeinhorn <ee237534@muhlenberg.edu>
2018-12-03 16:41:36 -0500

    Finished user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_031032) do
+ActiveRecord::Schema.define(version: 2018_12_03_212739) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_031032) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
