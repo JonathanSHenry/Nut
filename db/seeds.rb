a5362cbf46ec487328a9c49491172577cb3f80a2
eeinhorn <ee237534@muhlenberg.edu>
2018-12-03 23:41:11 -0500

    changed db:seed to add 10 users instead of 99; added "|" before delete for admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #seeds database with sample users
-User.create!(name:  "Example User",
+User.create!(name:  "Admin",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
 
-99.times do |n|
+10.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
