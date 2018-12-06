01e3cd297a2a4fd747a8c37b2338a1fb9b355161
eeinhorn <ee237534@muhlenberg.edu>
2018-12-06 17:24:17 -0500

    added proper restaurants to seed; started to change home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,11 +27,24 @@ User.create!(name:  "Example User",
                peanut: peanut,
                dairy: dairy,
                soy: soy)
-               
-  users = User.order(:created_at).take(6)
-  5.times do |n|
-    name = Faker::Name.name
-    description = Faker::Lorem.sentence(5)
-    users.each { |user| user.restaurants.create!(description: description) }
-  end
+
+restaurant_list = [
+  ["Luigi's Pizza", "Your basic pizza place.", "www.luigispizzafresca.com/fairmount.php", true, false, true, true, false, false],
+  ["Wendy's", "It's waaaay better than fast food. It's Wendy's.", "www.wendys.com", false, true, true, true, false, false],
+  ["McDonald's", "I'm lovin' it.", "www.mcdonalds.com/us/en-us.html", false, false, true, true, false, true],
+  ["Burger King", "Have It Your Way", "www.burgerking.com", true, false, false, true, false, true],
+  ["Emily's Latkes", "Hanukkah food all year round!", "www.potatoes.com", false, true, false, true, false, true],
+  ["Jon's Breakfast Sandwiches", "Eat them as you run to class!", "s3.amazonaws.com/pixtruder/original_images/d96d3393ab578175d3e38cbff9e940f4ec02183f", true, false, true, false, true, false ],
+  ["Stef's Pies", "What's your favorite kind of pie?", "images.midwestliving.mdpcdn.com/sites/midwestliving.com/files/styles/slide/public/Millionaires-Pie-101440700_0.jpg", false, true, true, false, false, true],
+  ["Lauren's Fries", "Gluten free and DELICIOUS!", "cdns.abclocal.go.com/content/kabc/images/cms/automation/vod/120318-fries.jpg", true, true, true, false, false, false],
+  ["Peter's Shakes", "Shakes so good, you'll scream!", "hips.hearstapps.com/del.h-cdn.co/assets/15/24/1600x800/landscape-1433889344-del-milkshakes-index.jpg?resize=480:*", false, false, false, true, true, true]
+]
+
+users = User.order(:created_at).take(1)
+  restaurant_list.each do |name, description, menu, peanut, gluten, dairy, egg, soy, shellfish|
+  users.each { |user| user.restaurants.create!(name: name, description: description, menu: menu,
+                                               peanut: peanut, gluten: gluten, dairy: dairy,
+                                               egg: egg, soy: soy, shellfish: shellfish) }
+end
+
 end
